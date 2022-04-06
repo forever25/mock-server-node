633a6068490a59379df464c117125ad633c70d97
forever25 <44657751+forever25@users.noreply.github.com>
2022-04-06 18:02:26 +0800

    revert: t

--- src/app/Modal.ts
+++ src/app/Modal.ts
@@ -1,35 +1,34 @@
 /*
  * @Author: ZWS
  * @Date: 2022-03-28 15:43:09
- * @LastEditTime: 2022-03-28 16:48:27
+ * @LastEditTime: 2022-04-06 18:01:35
  * @Description: 模型基本类
  */
 import fs from 'node:fs'
 
 export default class Modal {
-  public fileJson: string
+  public tableData:any
   public path: string
 
   constructor(path: string) {
     this.path = path;
-    this.fileJson = ''
-
+    this.tableData;
     this.readFile(this.path)
   }
   public readFile(path: string) {
     fs.readFile(path, { flag: 'r+', encoding: 'utf8' }, (err, data) => {
       if (err) {
-        fs.writeFile(path, '{}', { flag: 'a+', encoding: 'utf8' }, (err) => {
+        fs.writeFile(path, '[]', { flag: 'a+', encoding: 'utf8' }, (err) => {
         })
       }
-      this.fileJson = data;
+      this.tableData = JSON.parse(data) ;
     })
   }
   public isExists() {
 
   }
   public add() {
-
+    
   }
   public del() {
 
@@ -43,4 +42,5 @@ export default class Modal {
   public where() {
 
   }
+  
 }
--- src/models/ProjectModel.ts
+++ src/models/ProjectModel.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: ZWS
  * @Date: 2022-03-28 15:41:55
- * @LastEditTime: 2022-03-28 15:55:57
+ * @LastEditTime: 2022-04-06 18:01:38
  * @Description: 项目管理模型
  */
 import Modal from "@/app/Modal";
